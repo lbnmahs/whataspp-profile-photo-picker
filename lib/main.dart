dcf53f6466a23d4f91d686c5f624828777aec7ee
LbnMahs <labanmahs2002@gmail.com>
2024-01-17 14:27:16 +0300

    setting the theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
 }
+
+final colorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: Colors.blueGrey.shade500,
+  background: Colors.blueGrey.shade900
+);
+
+final theme = ThemeData().copyWith(
+  scaffoldBackgroundColor: colorScheme.background,
+  colorScheme: colorScheme,
+  textTheme: GoogleFonts.arvoTextTheme()
+);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -10,12 +23,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'WhatsApp Profile Photo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      theme: theme,
       home: const Text('Hello'),
     );
   }
 }
-
---
+++ lib/photo_picker.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ProfilePhotoPicker extends StatefulWidget {
+  const ProfilePhotoPicker({super.key});
+
+  @override
+  State<ProfilePhotoPicker> createState() {
+    return _ProfilePhotoPickerState();
+  }
+}
+
+class _ProfilePhotoPickerState extends State<ProfilePhotoPicker> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
